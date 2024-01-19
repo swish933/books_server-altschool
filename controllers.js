1e349241755a5f10f4ac1b770ddd88a449137b9c
Ikem Nomso <ikemnomso@gmail.com>
2024-01-19 21:48:20 +0100

    Delete book by Id

--- controllers.js
+++ controllers.js
@@ -58,8 +58,9 @@ const booksControllers = {
 		req.on("end", () => {
 			const parsedBody = Buffer.concat(body).toString();
 			const updateContent = JSON.parse(parsedBody);
+			const bookId = updateContent.id;
 
-			if (!updateContent.id) {
+			if (!bookId) {
 				res.writeHead(400);
 				res.end("No ID passed");
 			}
@@ -74,7 +75,7 @@ const booksControllers = {
 				const existingBooks = JSON.parse(books);
 
 				const bookIndex = existingBooks.findIndex((item) => {
-					return item.id === updateContent.id;
+					return item.id === bookId;
 				});
 
 				if (!existingBooks[bookIndex]) {
@@ -82,11 +83,13 @@ const booksControllers = {
 					res.end("Wrong ID");
 				}
 
+				//update books object
 				existingBooks[bookIndex] = {
 					...existingBooks[bookIndex],
 					...updateContent,
 				};
 
+				//Write updated books object to db
 				writeFile(booksDBPath, JSON.stringify(existingBooks), (err) => {
 					if (err) {
 						console.error(err);
@@ -98,7 +101,56 @@ const booksControllers = {
 			});
 		});
 	},
-	deleteBook: (req, res) => {},
+
+	deleteBook: (req, res) => {
+		const body = [];
+
+		req.on("data", (chunk) => {
+			body.push(chunk);
+		});
+
+		req.on("end", () => {
+			const parsedBody = Buffer.concat(body).toString();
+			const updateContent = JSON.parse(parsedBody);
+			const bookId = updateContent.id;
+
+			if (!bookId) {
+				res.writeHead(400);
+				res.end("No ID passed");
+			}
+
+			readFile(booksDBPath, "utf-8", (err, books) => {
+				if (err) {
+					console.error(err);
+					res.writeHead(500);
+					res.end("An error occured.");
+				}
+
+				const existingBooks = JSON.parse(books);
+
+				const bookIndex = existingBooks.findIndex((item) => {
+					return item.id === bookId;
+				});
+
+				if (!existingBooks[bookIndex]) {
+					res.writeHead(400);
+					res.end("Wrong ID");
+				}
+
+				//delete book with passed id from existing books object
+				existingBooks.splice(bookIndex, 1);
+
+				writeFile(booksDBPath, JSON.stringify(existingBooks), (err) => {
+					if (err) {
+						console.error(err);
+						res.writeHead(500);
+						res.end("An error occured.");
+					}
+					res.end("Delete successful");
+				});
+			});
+		});
+	},
 };
 
 module.exports = { booksControllers };
